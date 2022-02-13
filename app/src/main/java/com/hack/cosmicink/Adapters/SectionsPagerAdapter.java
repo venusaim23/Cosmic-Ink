22e64c662161f34f2d6bd20163b0f93dc6656f19
Venu Sai <venusaim23@gmail.com>
2022-02-13 14:54:09 +0530

    create fragments

--- app/src/main/java/com/hack/cosmicink/Adapters/SectionsPagerAdapter.java
+++ app/src/main/java/com/hack/cosmicink/Adapters/SectionsPagerAdapter.java
@@ -8,12 +8,18 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.hack.cosmicink.Fragments.ActionsFragment;
+import com.hack.cosmicink.Fragments.FollowFragment;
+import com.hack.cosmicink.Fragments.MessagesFragment;
+import com.hack.cosmicink.Fragments.QuestionsFragment;
+import com.hack.cosmicink.Fragments.TopicsFragment;
 import com.hack.cosmicink.R;
 
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2,
+            R.string.tab_text_3, R.string.tab_text_4, R.string.tab_text_5};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -23,8 +29,16 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
+        if (position == 0)
+            return new MessagesFragment();
+        if (position == 1)
+            return new TopicsFragment();
+        if (position == 2)
+            return new QuestionsFragment();
+        if (position == 3)
+            return new ActionsFragment();
+        if (position == 4)
+            return new FollowFragment();
         return null;
     }
 
---
+++ app/src/main/java/com/hack/cosmicink/Fragments/FollowFragment.java
@@ -0,0 +1,25 @@
+package com.hack.cosmicink.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hack.cosmicink.R;
+
+public class FollowFragment extends Fragment {
+
+    public FollowFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_follow, container, false);
+    }
+}
